8856c8e645889567746c05e3d6a71b7201cc7483
Thaddeus <thaddeusleezx@gmail.com>
2023-12-23 01:48:33 +0800

    fixed animations for side components

--- src/components/email.tsx
+++ src/components/email.tsx
@@ -8,18 +8,22 @@ export default function Email() {
         className="items-center relative rotate-90 translate-x-[7.14rem] -translate-y-80 text-rose-400 hidden xl:flex"
         id="email"
       >
-        <div
-          className="hover:-translate-x-2 hover:text-rose-500 transition"
+        <motion.div
+          className="hover:text-rose-500 transition"
+          animate={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 10 }}
+          whileHover={{ translateX: -6 }}
+          transition={{ duration: 0.05 }}
         >
           <motion.a
             href={`mailto:thaddeusleezx@gmail.com`}
             className="mt-20 mx-auto px-10 font-mono text-xxs leading-lg tracking-wide"
-            animate={{ opacity: 1, x: 0, transition: { duration: 0.2 } }}
-            initial={{ opacity: 0, x: 30 }}
+            // animate={{ opacity: 1, x: 0, y: 0,  transition: { duration: 1 } }}
+            // initial={{ opacity: 0, x: 30, y: 30 }}
           >
             thaddeusleezx@gmail.com
           </motion.a>
-        </div>
+        </motion.div>
       </div>
     </Side>
   );
--- src/components/return.tsx
+++ src/components/return.tsx
@@ -4,14 +4,18 @@ import { motion } from "framer-motion";
 
 export default function Return() {
   return (
-    <Side orientation="right" heightToAppear={3800}>
-      <Link href="#home" className="text-rose-500 text-5xl">
-        <motion.div
-          className="border-solid border border-t-0 border-r-4 border-b-4 border-l-0 inline-block p-2 rotate-[45deg] border-rose-500 hover:-translate-y-1 transition"
-          animate={{ rotate: 225 }}
-          transition={{ duration: 0.1 }}
-        />
-      </Link>
+    <Side orientation="right" heightToAppear={490}>
+      <div
+        className="mb-2 mr-8 hover:-translate-y-1"
+      >
+        <Link href="#home" className="text-rose-500 text-5xl">
+          <motion.div
+            className="border-solid border border-t-0 border-r-4 border-b-4 border-l-0 inline-block p-2 rotate-[45deg] border-rose-500"
+            animate={{ rotate: 225 }}
+            transition={{ duration: 0.2 }}
+          />
+        </Link>
+      </div>
     </Side>
   );
 }
